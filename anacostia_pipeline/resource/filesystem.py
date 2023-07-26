bc067d456bb51225f26eb12f8ad0400606018263
Minh Quan Do <mdo6180@gmail.com>
2023-07-26 13:18:24 -0400

    rebuilt ResourceNode and ActionNode to subclass BaseNode

--- anacostia_pipeline/resource/filesystem.py
+++ anacostia_pipeline/resource/filesystem.py
@@ -1,4 +1,3 @@
-import os
 import time
 import sys
 sys.path.append("../../anacostia_pipeline")
@@ -9,31 +8,21 @@ from watchdog.events import FileSystemEventHandler
 from engine.node import ResourceNode
 
 
-class DirChangeHandler(FileSystemEventHandler):
-    def __init__(self, node: ResourceNode) -> None:
-        self.node = node
-
-    # on modified event, send signal to parent nodes
-    # DirModifiedEvent or FileModifiedEvent is triggered because we are watching an existing directory 
-    # (i.e., the path argument in observer.schedule),
-    # if we were creating the directory, we would have to watch for DirCreatedEvent
-    def on_modified(self, event):
-        if event.is_directory:
-            print(f"Detected change: {event.event_type} {event.src_path}")
-            self.node.trigger()
-
-
-class DirWatchNode(ResourceNode):
+class DirWatchNode(ResourceNode, FileSystemEventHandler):
     def __init__(self, name, path):
         self.path = path
         self.name = name
-        super().__init__(name, "resource")
-        self.handler = DirChangeHandler(self)
+        super().__init__(name)
         self.observer = Observer()
+    
+    def on_modified(self, event):
+        if event.is_directory:
+            print(f"Detected change: {event.event_type} {event.src_path}")
+            self.trigger()
     
     def setup(self) -> None:
         print(f"Setting up node '{self.name}'")
-        self.observer.schedule(self.handler, path=self.path, recursive=False)
+        self.observer.schedule(self, path=self.path, recursive=False)
         self.observer.start()
         print("Observer started, waiting for file change...")
         print(f"Node '{self.name}' setup complete")
@@ -41,6 +30,7 @@ class DirWatchNode(ResourceNode):
     def teardown(self) -> None:
         self.observer.stop()
         self.observer.join()
+
 
 if __name__ == "__main__":
     folder1_node = DirWatchNode("folder1", "/Users/minhquando/Desktop/anacostia/anacostia_pipeline/resource/folder1")
