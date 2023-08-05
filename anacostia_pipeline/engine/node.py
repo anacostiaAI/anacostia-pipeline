35680739c72ff62d486d9e7736375a2a468ac14c
EC2 Default User <ec2-user@ip-172-31-81-112.ec2.internal>
2023-08-05 21:59:41 +0000

    starting development of new node

--- anacostia_pipeline/engine/node.py
+++ anacostia_pipeline/engine/node.py
@@ -1,9 +1,10 @@
-from multiprocessing import Queue
-from threading import Thread
-from typing import List, Any, Callable, Dict
+from multiprocessing import Queue, Process, Lock
+from typing import List, Any, Dict
 import time
 import networkx as nx
 from logging import Logger
+import uuid
+from uuid import UUID
 
 if __name__ == "__main__":
     from constants import Status
@@ -15,23 +16,23 @@ G = nx.DiGraph()
 
 
 class BaseNode:
-    queue = Queue()
 
     def __init__(self, 
         name: str, 
         signal_type: str,
-        action_function: Callable[..., Any] = None, 
         listen_to: List['BaseNode'] = [],
         auto_trigger: bool = False,
-        logger: Logger = None
     ) -> None:
 
         self.name = name
         self.signal_type = signal_type
-        self.action_function = action_function
         self.children = listen_to
         self.auto_trigger = auto_trigger
-        self.logger = logger
+
+        self.id = uuid.uuid4()
+        self.queue = Queue
+        self.logger = None
+        self.resource_lock = Lock()
 
         self.triggered = False
         self.signals_received = {child.get_name():Status.WAITING for child in self.children}
@@ -40,9 +41,12 @@ class BaseNode:
 
         for child in self.children:
             G.add_edge(child, self, signal_type=self.signal_type)
-
+    
     def __hash__(self) -> int:
-        return hash(self.name)
+        return hash(self.id)
+
+    def __repr__(self) -> str:
+        return f"'Node(name: {self.name}, uuid: {str(self.get_uuid())})'"
 
     def get_name(self) -> str:
         return self.name
@@ -50,13 +54,42 @@ class BaseNode:
     def get_queue(self) -> Queue:
         return self.queue
 
-    def setup(self) -> None:
+    def get_uuid(self) -> UUID:
+        return self.id
+    
+    def get_resource_lock(self) -> Lock:
+        # listening nodes can call this node's get_resource_lock() method inside a context manager to access resources like so:
+        # model_loader = ModelLoadingNode()
+        # with model_loader.get_resource_lock():
+        #     do something with the resource
+        return self.resource_lock
+    
+    def set_logger(self, logger: Logger) -> None:
+        # to be called by dag.py to set logger for all nodes
+        self.logger = logger
+    
+    def log(self, message: str) -> None:
         if self.logger is not None:
-            self.logger.info(f"Setting up node '{self.name}'")
-            self.logger.info(f"Node '{self.name}' setup complete")
+            self.logger.info(message)
         else:
-            print(f"Setting up node '{self.name}'")
-            print(f"Node '{self.name}' setup complete")
+            print(message)
+
+    def signal_message_template(self) -> Dict[str, Any]:
+        return {
+            "name": self.name,
+            "signal_type": self.signal_type,
+            "timestamp": time.time(),
+            "status": None
+        }
+
+    def setup(self) -> None:
+        # override to specify actions needed to create node.
+        # such actions can include pulling and setting up docker containers, 
+        # creating python virtual environments, creating database connections, etc.
+        # note that the setup() method will be ran in a separate thread; 
+        # this is the main difference between setting up the node using setup() and __init__()
+        # therefore, it is best to put set up logic here that is not dependent on other nodes.
+        pass
 
     def precheck(self) -> bool:
         # should be used for continuously checking if the node is ready to start
@@ -66,6 +99,10 @@ class BaseNode:
     def pre_execution(self) -> None:
         # override to enable node to do something before execution; 
         # e.g., send an email to the data science team to let everyone know the pipeline is about to train a new model
+        pass
+
+    def execute(self) -> None:
+        # the logic for a particular stage in the MLOps pipeline
         pass
 
     def on_success(self) -> None:
@@ -79,38 +116,30 @@ class BaseNode:
         pass
     
     def teardown(self) -> None:
-        print(f"Node '{self.name}' teardown complete")
+        # override to specify actions to be executed upon removal of node from dag or on pipeline shutdown
+        pass
     
-    def __repr__(self) -> str:
-        return f"'Node({self.name})'"
-
-    def signal_message_template(self) -> Dict[str, Any]:
-        return {
-            "name": self.name,
-            "signal_type": self.signal_type,
-            "timestamp": time.time(),
-            "status": None
-        }
+    def trigger(self) -> None:
+        # method called by user to manually trigger node
+        if self.precheck() is True:
+            self.triggered = True
 
     def __send_signal(self, status: Status) -> None:
-        print(f"Sending signal from node '{self.name}'")
+        self.log(f"Sending signal from node '{self.name}'")
         signal = self.signal_message_template()
         signal["status"] = status
         self.queue.put(signal)
     
-    def update_state(self) -> None:
-        pass
-    
     def __execution(self) -> bool:
         try:
-            if self.action_function is not None:
+            with self.resource_lock:
                 self.pre_execution()
-                self.action_function()
+                self.execute()
                 self.on_success()
             return True
         
         except Exception as e:
-            print(f"Node '{self.name}' execution failed: {e}")
+            self.log(f"Node '{self.name}' execution failed: {e}")
             self.on_failure()
             return False
 
@@ -119,9 +148,28 @@ class BaseNode:
         if len(self.children) > 0:
             self.signals_received = {child.get_name():Status.WAITING for child in self.children}
     
-    def __clear_queue(self, queue: Queue) -> Dict[str, Any]:
+    def poll_children(self) -> bool:
+        if len(self.children) == 0:
+            return True
+        else:
+            for child in self.children:
+                queue = child.get_queue()
+                signal = self.clear_queue(queue)
+            
+                if self.signals_received[child.get_name()] == Status.WAITING:
+                    self.signals_received[child.get_name()] = signal["status"]
+                    self.log(f"Received signal '{signal}' from node '{child.get_name()}'")
+            
+            if all(value == Status.SUCCESS for value in self.signals_received.values()) is True:
+                return True
+
+        return False
+
+    def clear_queue(self, queue: Queue) -> Dict[str, Any]:
         # sleep for a short time to allow queue to be populated
         # time.sleep(0.1)
+        # might want to consider making __clear_queue() a public method 
+        # because classes that override poll_children() might need to clear the queue
         signal = queue.get()
         while not queue.empty():
             if signal["status"] != Status.SUCCESS:
@@ -129,28 +177,6 @@ class BaseNode:
             else:
                 signal = queue.get()
         return signal
-
-    def poll_children(self) -> bool:
-        if len(self.children) == 0:
-            return True
-        else:
-            for child in self.children:
-                queue = child.get_queue()
-                signal = self.__clear_queue(queue)
-            
-                if self.signals_received[child.get_name()] == Status.WAITING:
-                    self.signals_received[child.get_name()] = signal["status"]
-                    print(f"Received signal '{signal}' from node '{child.get_name()}'")
-            
-            if all(value == Status.SUCCESS for value in self.signals_received.values()) is True:
-                return True
-
-        return False
-
-    def trigger(self) -> None:
-        # method called by user to manually trigger node
-        if self.precheck() is True:
-            self.triggered = True
 
     def __set_auto_trigger(self) -> None:
         if self.auto_trigger is True:
@@ -179,83 +205,20 @@ class BaseNode:
             self.teardown()
             exit(0)
 
+        except Exception as e:
+            self.log(f"Node '{self.name}' execution failed: {e}")
+
     def start(self) -> None:
-        print(f"Starting node '{self.name}'")
-        proc_listen = Thread(target=self.__run, daemon=True)
+        self.log(f"Starting node '{self.name}'")
+        proc_listen = Process(target=self.__run)
         proc_listen.start()
 
 
 class AndNode(BaseNode):
-    def __init__(self, listen_to: List['BaseNode']) -> None:
+    def __init__(self, name: str, signal_type: str, listen_to: List[BaseNode] = [], auto_trigger: bool = False) -> None:
         if len(listen_to) < 2:
-            raise ValueError("node '{name}' must be listening to at LEAST two nodes; listen_to must be provided with at least two nodes")
-        
-        super().__init__("AND", "AND", action_function=None, listen_to=listen_to, auto_trigger=True)
-
-
-class OrNode(BaseNode):
-    def __init__(self, listen_to: List['BaseNode']) -> None:
-        if len(listen_to) < 2:
-            raise ValueError("node '{name}' must be listening to at LEAST two nodes; listen_to must be provided with at least two nodes")
-
-        super().__init__("OR", "OR", action_function=None, listen_to=listen_to, auto_trigger=True)
-
-    def poll_children(self) -> bool:
-        for child in self.children:
-            queue = child.get_queue()
-            signal = self.__clear_queue(queue)
-        
-            if signal == Status.SUCCESS:
-                return True
-        
-        return False
-
-
-class ResourceNode(BaseNode):
-    def __init__(self, name: str, logger: Logger=None) -> None:
-        super().__init__(name, "resource", logger=logger)
-    
-    
-class ActionNode(BaseNode):
-    def __init__(self, 
-        name: str, 
-        signal_type: str, 
-        action_function: Callable[..., Any], 
-        listen_to: List['BaseNode'] = []
-    ) -> None:
-        
-        super().__init__(name, signal_type, action_function, listen_to, auto_trigger=True)
-
+            raise ValueError("AND node must be listening to more than two nodes")
+        super().__init__("AND", "AND", listen_to, auto_trigger)
 
 if __name__ == "__main__":
-    def resource1():
-        print("checking resource1")
-        time.sleep(1)
-        print("resource1 triggered")
-        return True
-    
-    def resource2():
-        print("checking resource2")
-        time.sleep(2)
-        print("resource2 triggered")
-        return True
-    
-    def train_model():
-        print("train_model triggered")
-        time.sleep(3)
-        print("train_model finished")
-        return True
-
-    node1 = ActionNode("resource1", "preprocess", resource1)
-    node2 = ActionNode("resource2", "load_model", resource2)
-    node3 = ActionNode("train_model", "train", train_model, listen_to=[node1, node2])
-
-    sorted_nodes = nx.topological_sort(G)
-    for node in sorted_nodes:
-        node.start()
-
-    time.sleep(10)
-
-    # we can use the to_dict_of_dicts function to output the graph as a json file
-    graph = nx.to_dict_of_dicts(G)
-    print(graph)
+    print("hello")
---
+++ tests/test_node.py
@@ -0,0 +1,49 @@
+import unittest
+import logging
+import sys
+import os
+import shutil
+import time
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../anacostia_pipeline'))
+
+from test_utils import get_log_messages
+from anacostia_pipeline.engine.node import BaseNode, AndNode
+
+if os.path.exists("./testing_artifacts") is False:
+    os.makedirs("./testing_artifacts")
+
+log_path = "./testing_artifacts/app.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename=log_path,
+    filemode='w'
+)
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+class NodeTests(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+    
+    def test_get_name(self):
+        node = BaseNode("base", "resource")
+        self.assertEqual(node.get_name(), "base")
+
+    def test_logger(self):
+        node = BaseNode("base", "resource")
+        node.set_logger(logger)
+        self.assertIs(node.logger, logger)
+
+        message = "hello there"
+        node.log(message)
+
+        log_messages = get_log_messages(log_path)
+        self.assertEqual(log_messages[0], message)
+
+if __name__ == "__main__":
+   unittest.main()
