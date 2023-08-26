2118d9235637a357c2c6734d3c67ce60e994171d
Minh Quan Do <mdo6180@gmail.com>
2023-08-26 17:09:31 -0400

    empty setup test is working for model registry

--- anacostia_pipeline/resource/model_registry.py
+++ anacostia_pipeline/resource/model_registry.py
@@ -1,60 +1,123 @@
 import sys
 import os
 import time
-from typing import Dict, List
+import json
+from datetime import datetime
+from functools import wraps
+
 sys.path.append("../../anacostia_pipeline")
+from engine.node import ResourceNode
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from engine.node import ResourceNode
-from engine.pipeline import Pipeline
-
 
 class ModelRegistryNode(ResourceNode, FileSystemEventHandler):
-    def __init__(self, name: str, path: str, num_old_models: int = 1, num_new_models: int = 1) -> None:
-        self.model_resgistry_path = os.path.join(path, "model_registry")
-        self.old_models_path = os.path.join(self.model_resgistry_path, "old_models")
-        self.new_models_path = os.path.join(self.model_resgistry_path, "new_models")
-        self.num_old_models = num_old_models
-        
-        self.last_checked_time = time.time()
-        
+    def __init__(self, name: str, path: str, framework: str) -> None:
+        self.model_registry_path = os.path.join(os.path.abspath(path), "model_registry")
+        self.model_registry_json_path = os.path.join(self.model_registry_path, "model_registry.json")
+        self.framework = framework
         self.observer = Observer()
+        self.setup_finished = False
         super().__init__(name, "model_registry")
 
+    def lock_decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            # make sure setup is finished before allowing access to resource
+            if func.__name__ == "setup":
+                with self.resource_lock:
+                    return func(self, *args, **kwargs)
+            else:
+                if self.setup_finished is True:
+                    with self.resource_lock:
+                        return func(self, *args, **kwargs)
+        return wrapper
+
+    @lock_decorator
     def setup(self) -> None:
-        os.makedirs(self.model_resgistry_path, exist_ok=True)
-        os.makedirs(self.old_models_path, exist_ok=True)
-        os.makedirs(self.new_models_path, exist_ok=True)
+        if os.path.exists(self.model_registry_path) is False:
+            os.makedirs(self.model_registry_path, exist_ok=True)
 
-        if self.logger is not None:
-            self.logger.info(f"Setting up node '{self.name}'")
-        else:
-            print(f"Setting up node '{self.name}'")
-        
-        self.observer.schedule(event_handler=self, path=self.new_models_path, recursive=True)
+        if os.path.exists(self.model_registry_json_path) is False:
+            with open(self.model_registry_json_path, 'w') as json_file:
+                json_entry = {
+                    "node": self.name,
+                    "resource_path": self.model_registry_path,
+                    "files": []
+                }
+
+                for filepath in os.listdir(self.model_registry_path):
+                    try:
+                        if filepath.endswith(".json") is False:
+                            path = os.path.join(self.model_registry_path, filepath)
+                            json_file_entry = {}
+                            json_file_entry["filepath"] = os.path.join(path)
+                            json_file_entry["framework"] = self.framework
+                            json_file_entry["state"] = "current"
+                            json_file_entry["created_at"] = str(datetime.now())
+                            json_entry["files"].append(json_file_entry)
+                    except Exception as e:
+                        self.log(f"Error loading feature vector file: {e}")
+                        continue
+
+                json.dump(json_entry, json_file, indent=4)
+
+        self.log(f"Setting up node '{self.name}'")
+        self.observer.schedule(event_handler=self, path=self.model_registry_path, recursive=True)
         self.observer.start()
+        self.log(f"Node '{self.name}' setup complete. Observer started, waiting for file change...")
+        self.setup_finished = True
 
-        if self.logger is not None:
-            self.logger.info(f"Node '{self.name}' setup complete. Observer started, waiting for file change...")
-        else:
-            print(f"Node '{self.name}' setup complete. Observer started, waiting for file change...")
-
+    @lock_decorator
     def on_modified(self, event):
-        with self.resource_lock:
-            if event.is_directory:
-                if self.logger is not None:
-                    for model_paths in self.new_models_paths():
-                        self.logger.info(f"New models detected: {event.event_type} {model_paths}")
-                else:
-                    for model_paths in self.new_models_paths():
-                        print(f"New models detected: {event.event_type} {model_paths}")
+        if not event.is_directory:
+            with open(self.model_registry_json_path, 'r') as json_file:
+                json_data = json.load(json_file)
+            
+            try:
+                # change of direction: use the on_modified method to monitor the change of the model_registry.json file
+                # once we see a new model, we can trigger the next node
+                logged_files = [entry["filepath"] for entry in json_data["files"]]
+                if (event.src_path.endswith(".json") is False) and (event.src_path not in logged_files):
+                    json_entry = {}
+                    json_entry["filepath"] = event.src_path
+                    json_entry["framework"] = self.framework
+                    json_entry["state"] = "new"
+                    json_entry["created_at"] = str(datetime.now())
+                    json_data["files"].append(json_entry)
 
-                # make sure signal is created before triggering
-                self.trigger()
+            except Exception as e:
+                self.log(f"Error processing {event.src_path}: {e}")
+            
+            with open(self.model_registry_json_path, 'w') as json_file:
+                json.dump(json_data, json_file, indent=4)
 
-            self.last_checked_time = time.time()
+            # make sure signal is created before triggering
+            self.trigger()
+
+    def create_filename(self, file_extension: str = None) -> str:
+        """
+        Default implementaion to create a filename for the new feature vector file.
+        Method can be overridden to create a custom filename; but user must ensure that the filename is unique.
+        """
+
+        if file_extension is None:
+            if self.framework == "tensorflow":
+                file_extension = "h5"
+            elif self.framework == "pytorch":
+                file_extension = "pt"
+            elif self.framework == "sklearn":
+                file_extension = "pkl"
+            elif self.framework == "onnx":
+                file_extension = "onnx"
+            else:
+                raise ValueError(
+                    f"Framework '{self.framework}' not supported. If you want to use this framework, please specify a file extension."
+                )
+
+        num_files = len(os.listdir(self.model_registry_path))
+        return f"model_{num_files}.{file_extension}"
 
     """
     def load_old_model(self, path: str) -> None:
@@ -71,7 +134,6 @@ class ModelRegistryNode(ResourceNode, FileSystemEventHandler):
         for i, model_path in enumerate(self.old_models_paths()):
             if i < self.num_old_models:
                 yield self.load_old_model(model_path)
-    """
 
     def old_models_paths(self) -> None:
         models = os.listdir(self.old_models_path)
@@ -80,22 +142,39 @@ class ModelRegistryNode(ResourceNode, FileSystemEventHandler):
     def new_models_paths(self) -> None:
         models = os.listdir(self.new_models_path)
         return models
-    
-    def save_model(self, model_name: str) -> None:
-        print(f"saving model {model_name} to registry {self.model_resgistry_path}")
-        time.sleep(1)
-        print(f"model {model_name} saved")
+    """
 
-    def teardown(self) -> None:
+    @lock_decorator 
+    def save_model(self) -> None:
+        raise NotImplementedError
+
+    def update_state(self):
+        # create another decorator to call event.wait() multiple times
+        for _ in range(self.num_successors):
+            self.event.wait()
+
+        with self.resource_lock:
+            with open(self.model_registry_json_path, 'r') as json_file:
+                json_data = json.load(json_file)
+
+            for file_entry in json_data["files"]:
+                if file_entry["state"] == "current":
+                    self.log(f'current -> old: {file_entry["filepath"]}')
+                    file_entry["state"] = "old"
+            
+            for file_entry in json_data["files"]:
+                if file_entry["state"] == "new":
+                    self.log(f'new -> current: {file_entry["filepath"]}')
+                    file_entry["state"] = "current"
+
+            with open(self.model_registry_json_path, 'w') as json_file:
+                json.dump(json_data, json_file, indent=4)
+        
+        if self.event.is_set():
+            self.event.clear()
+    
+    def on_exit(self) -> None:
+        self.log(f"Beginning teardown for node '{self.name}'")
         self.observer.stop()
         self.observer.join()
-        if self.logger is not None:
-            self.logger.info(f"Node '{self.name}' teardown complete.")
-        else:
-            print(f"Node '{self.name}' teardown complete.")
-
-
-if __name__ == "__main__":
-    model_registry_node = ModelRegistryNode(name="model_registry", path="../../tests/testing_artifacts")
-    dag = Pipeline([model_registry_node])
-    dag.start()
+        self.log(f"Observer stopped for node '{self.name}'")
--- tests/test_model_registry.py
+++ tests/test_model_registry.py
@@ -3,7 +3,7 @@ import logging
 import sys
 import os
 import shutil
-import time
+import random
 from threading import Thread
 
 sys.path.append('..')
@@ -14,8 +14,18 @@ from anacostia_pipeline.engine.constants import Status
 from test_utils import *
 
 
-log_path = "./testing_artifacts/app.log"
+# Set the seed for reproducibility
+seed_value = 42
+random.seed(seed_value)
 
+model_registry_tests_path = "./testing_artifacts/model_registry_tests"
+if os.path.exists(model_registry_tests_path) is True:
+    shutil.rmtree(model_registry_tests_path)
+
+os.makedirs(model_registry_tests_path)
+os.chmod(model_registry_tests_path, 0o777)
+
+log_path = f"{model_registry_tests_path}/model_registry.log"
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -28,36 +38,52 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class PyTorchModelRegistryNode(ModelRegistryNode):
+    def __init__(self, name: str, path: str) -> None:
+        super().__init__(name, path, "pytorch")
+
+    def create_filename(self) -> str:
+        return super().create_filename("pt")
+    
+    def save_model(self) -> None:
+        model_name = self.create_filename()
+        create_file(f"{self.model_registry_path}/{model_name}", "PyTorch model")
+        self.log(f"saved model {model_name} to registry {self.model_registry_path}")
+
+
 class NodeTests(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
-        if os.path.exists("./testing_artifacts") is False:
-            os.makedirs("./testing_artifacts")
-            os.chmod("./testing_artifacts", 0o777)
-
-        self.model_registry_node = ModelRegistryNode(name="model_registry", path="./testing_artifacts")
-        self.model_registry_node.set_logger(logger)
-        self.model_registry_node.set_status(Status.RUNNING)
-        self.model_registry_node.setup()
-
-        self.thread = Thread(target=self.model_registry_node.run)
-        self.thread.start()
-    
         super().__init__(methodName)
 
-    def test_setup(self):
-        self.assertTrue(os.path.exists("./testing_artifacts/model_registry"))
-        self.assertTrue(os.path.exists("./testing_artifacts/model_registry/old_models"))
-        self.assertTrue(os.path.exists("./testing_artifacts/model_registry/new_models"))
+    def setUp(self) -> None:
+        self.feature_store_dirs = os.listdir(model_registry_tests_path)
+        self.path = f"{model_registry_tests_path}/{self._testMethodName}"
+        os.makedirs(self.path)
 
-    def tearDown(self) -> None:
-        self.model_registry_node.set_status(Status.STOPPING)
-        self.thread.join()
-        self.model_registry_node.teardown()
+    def start_node(self, model_registry_node: PyTorchModelRegistryNode) -> None:
+        model_registry_node.set_logger(logger)
+        model_registry_node.num_successors = 1
+        model_registry_node.start()
 
-        try:
-            shutil.rmtree("./testing_artifacts/model_registry")
-        except OSError as e:
-            print(f"Error occurred: {e}")
+    def tearDown_node(self, model_registry_node: PyTorchModelRegistryNode) -> None:
+        model_registry_node.stop()
+        model_registry_node.join()
+
+    def test_empty_setup(self):
+        model_registry_node = PyTorchModelRegistryNode(name=f"{self._testMethodName}", path=self.path)
+        self.start_node(model_registry_node)
+        
+        # wait for node to finish setup
+        time.sleep(0.1)
+        
+        for _ in range(5):
+            model_registry_node.save_model()
+            time.sleep(0.1)
+
+        time.sleep(0.1)
+        model_registry_node.event.set()
+        self.tearDown_node(model_registry_node)
+
 
 if __name__ == "__main__":
     unittest.main()
