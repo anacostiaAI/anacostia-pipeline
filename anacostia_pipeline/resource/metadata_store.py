167d1f29b5500a89da6df42ba8c9006035d5bf7e
Minh Quan Do <mdo6180@gmail.com>
2023-09-01 03:36:02 -0400

    removed a lot of code from metadata store and converted the class into a template

--- anacostia_pipeline/resource/metadata_store.py
+++ anacostia_pipeline/resource/metadata_store.py
@@ -1,17 +1,16 @@
 from anacostia_pipeline.engine.node import ResourceNode
-from anacostia_pipeline.resource.model_registry import ModelRegistryNode
-from anacostia_pipeline.resource.feature_store import FeatureStoreNode
-from anacostia_pipeline.resource.data_store import DataStoreNode
-
 from sqlalchemy import create_engine, Integer, String, Float
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Session
+
 
 class Base(DeclarativeBase):
     pass
 
+
 class MetadataTable(Base):
     __tablename__ = "metadata"
 
+    # these fields are good
     id = mapped_column(Integer, primary_key=True)
     train_acc = mapped_column(Float)
     valid_acc = mapped_column(Float)
@@ -20,66 +19,55 @@ class MetadataTable(Base):
     model_path = mapped_column(String)
     features_path = mapped_column(String)
     data_path = mapped_column(String)
+    state = mapped_column(String)
 
 
 class MetadataStoreNode(ResourceNode):
     '''A ResourceNode responsible for saving training and model evaluation data into a persistant store'''
     
-    def __init__(self, model_reg:ModelRegistryNode, feat_store:FeatureStoreNode, data_store:DataStoreNode, path:str="sqlite:///default_metadata.db"):
+    def __init__(self, name: str, path: str = "sqlite:///default_metadata.db"):
         self.engine = create_engine(path)
 
         # Automatically create the table if it doesn't exist
-        Base.metadata.create_all(engine)
+        Base.metadata.create_all(self.engine)
+        super().__init__(name, "metadata_store")
 
-        # TODO add as parameters and set here once they are defined
-        self.train_node = None
-        self.model_eval = None
+    @ResourceNode.resource_accessor
+    def setup(self) -> None:
+        # TODO: implement a function that installs the database, 
+        # spin up the database, 
+        # and opens a connection to the database when the node is started
+        pass
+    
+    @ResourceNode.exeternally_accessible
+    @ResourceNode.resource_accessor
+    def insert_metadata(self, **kwargs):
+        # TODO: implement a function that inserts the metadata into the database
+        # users will use kwargs to pass in the metadata
+        pass
 
-        self.model_reg = model_reg
-        self.feat_store = feat_store
-        self.data_store = data_store
-
-    @ResourceNode.lock_decorator
-    def insert_metadata(self, train_acc:float=0, valid_acc:float=0, test_acc:float=0, epoch:int=0):
-        '''
-        Inserts a row into the metadata table
-        :param train_acc: Training Accuracy
-        :param valid_acc: Validation Accuracy
-        :param test_acc: Testing Accuracy
-        :param epoch: Number of Epochs that Training had (I Think?)
-        '''
-        with Session(engine) as s:
-            s.add(MetadataTable(
-                train_acc=train_acc,
-                valid_acc=valid_acc,
-                test_acc=test_acc,
-                epoch=epoch,
-                model_path=self.model_registry_path,
-                features_path=self.feat_store.feature_store_path,
-                data_path=self.data_store.data_store_path
-            ))
-        s.commit()
-
-
-    def execute(self):
-        '''
-        Returns True if an insertion into the database was successful
-        '''
-        if not self.train_node:
-            return False
-
-        # TODO update once a training node is defined
-        epoch = self.train_node.epoch 
+    @ResourceNode.exeternally_accessible
+    @ResourceNode.resource_accessor
+    def trigger_condition(self) -> bool:
+        raise NotImplementedError
+    
+    @ResourceNode.exeternally_accessible
+    @ResourceNode.resource_accessor
+    def get_metadata(self, state: str) -> iter:
+        if state not in ["current", "old", "new", "all"]:
+            raise ValueError("state must be one of ['current', 'old', 'new', 'all']")
         
-        if not self.model_eval:
-            return False
+        # TODO: implement functions for retrieveing metadata from the database based on the state
+        # Note: this function should be an iterator, see load_data_samples and load_data_samples in data_store.py
+        pass
 
-        # TODO update once a model_eval node is defined
-        train_acc = self.model_eval.train_acc
-        valid_acc = self.model_eval.valid_acc
-        test_acc = self.model_eval.test_acc
-
-        self.insert_metadata(train_acc, valid_acc, test_acc, epoch)
+    @ResourceNode.await_references
+    @ResourceNode.resource_accessor
+    def execute(self):
+        # TODO: implement a function that updates the state of the metadata store
+        # i.e., move the current metadata to old, and the new metadata to current
         return True
 
-
+    def on_exit(self) -> None:
+        # TODO: implement a function that closes the database connection when the node is terminated
+        pass
