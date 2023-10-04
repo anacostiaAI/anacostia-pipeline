495b658504818185770fbabf8234a64aa7210957
Minh Quan Do <mdo6180@gmail.com>
2023-10-04 18:42:20 -0400

    added code for action node thread and resource node thread in redesign

--- anacostia_pipeline/engine/base.py
+++ anacostia_pipeline/engine/base.py
@@ -8,6 +8,7 @@ from functools import wraps
 import traceback
 import sys
 from pydantic import BaseModel
+from networkx import DiGraph
 
 if __name__ == "__main__":
     from constants import Status
@@ -33,6 +34,15 @@ class BaseNode(Thread):
         self.successors_queue = Queue()
         self.received_predecessors_signals: Dict[str, Message] = dict()
         self.received_successors_signals: Dict[str, Message] = dict()
+
+    def insert_node(self, G: DiGraph) -> None:
+        for predecessor in self.predecessors:
+            if G.has_edge(predecessor, self) is False:
+                G.add_edge(predecessor, self)
+
+        for successor in self.successors:
+            if G.has_edge(self, successor) is False:
+                G.add_edge(self, successor)
 
     def __hash__(self) -> int:
         return hash(self.name)
@@ -61,7 +71,7 @@ class BaseNode(Thread):
                 break
 
     @staticmethod
-    def trap(func):
+    def trap_interrupts(func):
         '''
         A Decorator for allowing the node to be paused mid execution
         '''
@@ -84,7 +94,7 @@ class BaseNode(Thread):
             return ret
         return wrapper
 
-    def user_defined(func):
+    def trap_exceptions(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
             try: 
@@ -92,7 +102,7 @@ class BaseNode(Thread):
                 return ret
             except Exception as e:
                 print(f"Error in user-defined method '{func.__name__}' of node '{self.name}': {traceback.format_exc()}")
-                # self.status = Status.ERROR
+                self.status = Status.ERROR
                 return
         return wrapper
     
@@ -102,7 +112,6 @@ class BaseNode(Thread):
             timestamp = datetime.now(),
             status = status
         )
-
         for successor in self.successors:
             successor.predecessors_queue.put(msg)
 
@@ -112,7 +121,6 @@ class BaseNode(Thread):
             timestamp = datetime.now(),
             status = status
         )
-
         for predecessor in self.predecessors:
             predecessor.successors_queue.put(msg)
 
@@ -181,6 +189,7 @@ class BaseNode(Thread):
     def stop(self):
         self.status = Status.EXITING
 
+    @trap_exceptions
     def setup(self) -> None:
         """
         override to specify actions needed to create node.
@@ -192,6 +201,7 @@ class BaseNode(Thread):
         """
         pass
 
+    @trap_exceptions
     def on_exit(self):
         """
         on_exit is called when the node is being stopped.
@@ -205,10 +215,7 @@ class BaseResourceNode(BaseNode):
     def __init__(
         self, name: str, 
         successors: List['BaseActionNode'],
-        auto_trigger: bool = False
     ) -> None:
-        self.auto_trigger = auto_trigger
-        self.triggered = auto_trigger
         self.resource_lock = RLock()
         super().__init__(name, [], successors)
 
@@ -230,12 +237,16 @@ class BaseResourceNode(BaseNode):
                     return func(self, *args, **kwargs)
         return wrapper
 
-    @BaseNode.trap
+    @BaseNode.trap_interrupts
+    @BaseNode.trap_exceptions
     @resource_accessor
     def update_state(self):
-        pass
+        """
+        override to specify how the state of the resource is updated
+        """
+        raise NotImplementedError
 
-    @BaseNode.trap
+    @BaseNode.trap_interrupts
     @resource_accessor
     def check_resource(self) -> bool:
         return True
@@ -244,12 +255,7 @@ class BaseResourceNode(BaseNode):
         # --------------------------- iteration 0 (initialization) ---------------------------
         # setting up the node and the resource
         self.status = Status.INIT
-        try:
-            self.setup()
-        except Exception as e:
-            print(f"Error setting up node '{self.name}': {traceback.format_exc()}")
-            self.status = Status.ERROR
-            return
+        self.setup()
     
         # waiting for the trigger condition to be met
         self.status = Status.WAITING_RESOURCE
@@ -257,6 +263,8 @@ class BaseResourceNode(BaseNode):
             try:
                 if self.check_resource() is True:
                     break
+                else:
+                    time.sleep(0.1)
             except Exception as e:
                 print(f"Error checking resource in node '{self.name}': {traceback.format_exc()}")
                 continue
@@ -266,12 +274,7 @@ class BaseResourceNode(BaseNode):
         
         # updating the state of the resource in case the trigger condition is met in iteration 0
         self.status = Status.UPDATE_STATE
-        try:
-            self.update_state()
-        except Exception as e:
-            print(f"Error updating state in node '{self.name}': {traceback.format_exc()}")
-            self.status = Status.ERROR
-            return
+        self.update_state()
 
         self.signal_successors(Status.SUCCESS)
         # --------------------------- end of iteration 0 (initialization phase) ---------------------------
@@ -281,16 +284,16 @@ class BaseResourceNode(BaseNode):
 
             # check the resource to see if the trigger condition is met, and if so, signal the next node
             self.status = Status.WAITING_RESOURCE
+            resource_check = False
             try:
                 resource_check = self.check_resource()
             except Exception as e:
                 # Note: the only function that should throw an exception is check_resource() because it is a user-defined function
                 # the functions that we've created should not throw exceptions
                 print(f"Error checking resource in node '{self.name}': {traceback.format_exc()}")
-                resource_check = False
 
             # signal the successors to execute if the trigger condition is met
-            self.signal_successors(Status.SUCCESS if resource_check is True else Status.FAILURE)
+            self.signal_successors(Status.SUCCESS if resource_check else Status.FAILURE)
 
             # check for successors signals before updating state to ensure all successors have finished using the current state
             self.status = Status.WAITING_SUCCESSORS
@@ -314,21 +317,11 @@ class BaseActionNode(BaseNode):
         name: str,
         predecessors: List[BaseNode], 
         successors: List['BaseActionNode'],
-        auto_trigger: bool = False
     ) -> None:
-        self.auto_trigger = auto_trigger
-        self.triggered = auto_trigger
         super().__init__(name, predecessors, successors)
 
-    @BaseNode.trap
-    def on_failure(self, e: Exception = None) -> None:
-        """
-        override to enable node to do something after execution in event of failure of action_function; 
-        e.g., send an email to the data science team to let everyone know the pipeline has failed to train a new model
-        """
-        pass
-    
-    @BaseNode.trap
+    @BaseNode.trap_interrupts
+    @BaseNode.trap_exceptions
     def pre_execution(self) -> None:
         """
         override to enable node to do something before execution; 
@@ -336,22 +329,33 @@ class BaseActionNode(BaseNode):
         """
         pass
 
-    @BaseNode.trap
+    @BaseNode.trap_interrupts
     def execute(self, *args, **kwargs) -> bool:
         """
         the logic for a particular stage in your MLOps pipeline
         """
-        return True
+        raise NotImplementedError
 
-    @BaseNode.trap
-    def post_execution(self) -> None:
+    @BaseNode.trap_interrupts
+    @BaseNode.trap_exceptions
+    def on_failure(self) -> None:
         """
-        override to enable node to do something after execution; 
-        e.g., send an email to the data science team to let everyone know the pipeline is has trained a new model
+        override to enable node to do something after execution in event of failure of action_function; 
+        e.g., send email to the data science team to let everyone know the pipeline has failed to train a new model
         """
         pass
     
-    @BaseNode.trap
+    @BaseNode.trap_interrupts
+    @BaseNode.trap_exceptions
+    def on_error(self, e: Exception) -> None:
+        """
+        override to enable node to do something after execution in event of error of action_function; 
+        e.g., send email to the data science team to let everyone know the pipeline has failed to train a new model due to an error
+        """
+        pass
+    
+    @BaseNode.trap_interrupts
+    @BaseNode.trap_exceptions
     def on_success(self) -> None:
         """
         override to enable node to do something after execution in event of success of action_function; 
@@ -359,70 +363,44 @@ class BaseActionNode(BaseNode):
         """
         pass
 
-    def trigger(self) -> None:
-        self.triggered = True
-
-    def reset_trigger(self):
-        # TODO reset trigger dependent on the state of the system i.e. data store, feature store, model store
-        if self.auto_trigger == False:
-            self.triggered = False
-
     def run(self) -> None:
+        # --------------------------- iteration 0 (initialization) ---------------------------
+        # setting up the node and the resource
         self.status = Status.INIT
-        try:
-            self.setup()
-        except Exception as e:
-            print(f"Error setting up node '{self.name}': {traceback.format_exc()}")
-            self.status = Status.ERROR
-            return
+        self.setup()
+        # --------------------------- end of iteration 0 (initialization phase) ---------------------------
 
+        # --------------------------- iteration 1+ (monitoring phase) -------------------------------------
         while True:
-            if self.check_predecessors_signals() is False:
-                self.status = Status.WAITING
-                continue
-            else:
-                self.status = Status.RUNNING
+            self.status = Status.WAITING_PREDECESSORS
+            while self.check_predecessors_signals() is False:
+                time.sleep(0.1)
+            
+            self.status = Status.RUNNING
+            self.pre_execution()
+            ret = False
+            try:
+                ret = self.execute()
+                if ret:
+                    self.status = Status.SUCCESS
+                    self.on_success()
+                else:
+                    self.status = Status.FAILURE
+                    self.on_failure()
 
-            if self.status == Status.RUNNING:               
+            except Exception as e:
+                print(f"Error executing node '{self.name}': {traceback.format_exc()}")
+                self.on_error(e)
+                self.status = Status.ERROR
+                return
 
-                if self.triggered:
-                    self.pre_execution()
+            self.signal_successors(Status.SUCCESS if ret else Status.FAILURE)
 
-                    try:
-                        ret = self.execute()
-                        if ret:
-                            self.on_success()
-                            self.post_execution()
-                            self.signal_successors(Status.SUCCESS)
-                            self.signal_predecessors(Status.SUCCESS)
-
-                        else:
-                            self.on_failure()
-                            self.post_execution()
-                            self.signal_successors(Status.FAILURE)
-                            self.signal_predecessors(Status.FAILURE)
-
-                    except Exception as e:
-                        print(f"Error in execution method of node '{self.name}': {traceback.format_exc()}")
-                        # what happens if the user-defined on_failure method also throws an exception?
-                        # TODO: add a try-except block around the on_failure method
-                        self.on_failure(e)
-                        self.post_execution()
-                        self.signal_successors(Status.FAILURE)
-                        self.signal_predecessors(Status.FAILURE)
-
-                    self.reset_trigger()    
-                    time.sleep(0.1)
-
-            elif self.status == Status.PAUSED:
-                # Stay Indefinitely Paused until external action
+            # checking for successors signals before signalling predecessors will 
+            # ensure all action nodes have finished using the current state
+            self.status = Status.WAITING_SUCCESSORS
+            while self.check_successors_signals() is False:
                 time.sleep(0.1)
 
-            elif self.status == Status.STOPPING:
-                # TODO: what happens if the user-defined on_exit method throws an exception?
-                # TODO: add a try-except block around the on_exit method and change status to Status.EXITED if exception is thrown
-                self.on_exit()
-                self.status = Status.EXITED
-                return
-            
-            time.sleep(0.1)
+            self.signal_predecessors(Status.SUCCESS if ret else Status.FAILURE)
+        # --------------------------- iteration 1+ (monitoring phase) ---------------------------------------
