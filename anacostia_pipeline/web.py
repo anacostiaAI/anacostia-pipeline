cf5d61e349fa47dd3a7d3d0ba9d81d5f67b2a817
Minh Quan Do <mdo6180@gmail.com>
2023-12-05 04:10:01 -0500

    added frontend with htmx + FastAPI + Jinja2

--- anacostia_pipeline/web.py
+++ anacostia_pipeline/web.py
@@ -1,13 +1,20 @@
 from importlib import metadata
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request, Response
 
-from anacostia_pipeline.engine.base import BaseNode, BaseMetadataStoreNode, NodeModel
-from anacostia_pipeline.engine.pipeline import Pipeline, PipelineModel
+from engine.base import BaseNode, BaseMetadataStoreNode, NodeModel
+from engine.pipeline import Pipeline, PipelineModel
 
 PACKAGE_NAME="anacostia_pipeline"
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+templates = Jinja2Templates(directory="./templates")
+
 
 # For now, Assuming we are running 1 Pipeline
 n1=BaseMetadataStoreNode("test_node1")
@@ -35,3 +42,25 @@ def nodes(name: str) -> NodeModel:
     Returns information on a given Node by name
     '''
     return p[name].model()
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    nodes = [
+        {"name": "preprocessing", "endpoint": "/preprocessing"},
+        {"name": "training", "endpoint": "/training"},
+        {"name": "evaluation", "endpoint": "/evaluation"},
+    ]
+    return templates.TemplateResponse("base.html", {"request": request, "nodes": nodes, "title": "Anacostia Pipeline"})
+
+@app.get("/preprocessing", response_class=HTMLResponse)
+def preprocessing():
+    return '<h1 class="blue-text">preprocessing</h1>'
+
+@app.get("/training", response_class=HTMLResponse)
+def training():
+    return "<h1>training</h1>"
+
+@app.get("/evaluation", response_class=HTMLResponse)
+def evaluation():
+    return "<h1>evaluation</h1>"
