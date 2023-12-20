9af40dd6661df8f4efb0ed358d55bb7c34b929bf
Minh Quan Do <mdo6180@gmail.com>
2023-12-20 17:20:34 -0500

    created new frontend that doesn't block main pipeline execution

---
+++ anacostia_pipeline/frontend.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import signal
+from importlib import metadata
+from threading import Thread
+import uvicorn
+
+from jinja2.filters import FILTERS
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+from .engine.base import NodeModel, BaseMetadataStoreNode, BaseResourceNode, BaseActionNode
+from .engine.pipeline import Pipeline, PipelineModel
+
+
+
+PACKAGE_NAME = "anacostia_pipeline"
+PACKAGE_DIR = os.path.dirname(sys.modules[PACKAGE_NAME].__file__)
+
+# Additional Filters for Jinja Templates
+def basename(value, attribute=None):
+    return os.path.basename(value)
+
+def type_name(value):
+    return type(value).__name__
+
+FILTERS['basename'] = basename
+FILTERS['type'] = type_name
+
+
+
+class Webserver(FastAPI):
+    def __init__(self, pipeline: Pipeline, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pipeline = pipeline
+        self.static_dir = os.path.join(PACKAGE_DIR, "static")
+        self.templates_dir = os.path.join(PACKAGE_DIR, "templates")
+
+        self.mount("/static", StaticFiles(directory=self.static_dir), name="static")
+        self.templates = Jinja2Templates(directory=self.templates_dir)
+
+        @self.get('/api/')
+        def welcome():
+            return {
+                "package": PACKAGE_NAME,
+                "version": metadata.version(PACKAGE_NAME)
+            }
+
+        @self.get('/api/pipeline/')
+        def pipeline() -> PipelineModel:
+            '''
+            Returns information on the pipeline
+            '''
+            return self.pipeline.model()
+
+        @self.get('/', response_class=HTMLResponse)
+        async def hello(request: Request):
+            #self.pipeline.launch_nodes()
+            nodes = self.pipeline.model().nodes
+            return self.templates.TemplateResponse("base.html", {"request": request, "nodes": nodes, "title": "Anacostia Pipeline"})
+
+        @self.get('/node', response_class=HTMLResponse)
+        async def node_html(request: Request, name: str, property: str=None):
+            node = self.pipeline[name]
+            if node is None:
+                return "<h1>Node Not Found</h1>"
+            
+            if property is not None:
+                n = node.model().dict()
+                return n.get(property, "")
+
+            # TODO update this so all node types have a .model() that
+            # returns a pydantic BaseModel Type to streamlize serialization of nodes
+            if isinstance(node, BaseMetadataStoreNode):
+                return node.html(self.templates, request)
+
+            if isinstance(node, BaseResourceNode):
+                return node.html(self.templates, request)
+
+            if isinstance(node, BaseActionNode):
+                return node.html(self.templates, request)
+
+            n = node.model()
+            return n.view(self.templates, request)
+
+
+    
+def run_background_webserver(pipeline: Pipeline, **kwargs):
+    app = Webserver(pipeline)
+    config = uvicorn.Config(app, **kwargs)
+    server = uvicorn.Server(config)
+    fastapi_thread = Thread(target=server.run)
+
+    original_sigint_handler = signal.getsignal(signal.SIGINT)
+
+    def _kill_pipeline(sig, frame):
+        print("\nCTRL+C Caught!; Killing Pipeline...")
+        pipeline.terminate_nodes()
+
+        # register the original default kill handler once the pipeline is killed
+        signal.signal(signal.SIGINT, original_sigint_handler)
+        print("Pipeline Killed.")
+    
+    def _kill_webserver(sig, frame):
+        print("\nCTRL+C Caught!; Killing Webserver...")
+        server.should_exit = True
+        fastapi_thread.join()
+
+        # register the kill handler for the pipeline once the webserver is killed
+        signal.signal(signal.SIGINT, _kill_pipeline)
+        print("Webserver Killed; press CTRL+C again to kill pipeline...")
+
+    # launch the pipeline
+    print("Launching Pipeline...")
+    pipeline.launch_nodes()
+
+    # register the kill handler for the webserver
+    signal.signal(signal.SIGINT, _kill_webserver)
+    fastapi_thread.start()
+
