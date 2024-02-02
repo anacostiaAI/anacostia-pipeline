ac5bfb33d1ac0c08659287dcf05f4a60599fcf24
Minh Quan Do <mdo6180@gmail.com>
2024-02-01 19:03:55 -0500

    packaged project and uploaded on pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup, find_packages
+import os
+import fnmatch
+
+
+def package_files(directory):
+    """
+    Recursively collects file paths within a directory relative to the anacostia_pipeline directory.
+    """
+    anacostia_pipeline_dir = os.path.abspath("anacostia_pipeline")
+    paths = []
+    exclude = ["*__pycache__*", "*.pyc", "*.pyo", "*.DS_Store"]
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            pathname = os.path.relpath(os.path.join(root, filename), anacostia_pipeline_dir)
+            if not any([fnmatch.fnmatch(pathname, pattern) for pattern in exclude]):
+                paths.append(pathname)
+    return paths
+
+static_files = package_files("anacostia_pipeline/static")
+template_files = package_files("anacostia_pipeline/templates")
+
 
 setup(
     name="anacostia_pipeline",
@@ -8,7 +29,11 @@ setup(
     author_email="mdo9@gmu.edu",
     packages=find_packages(),
     package_data={
-        'anacostia_pipeline': ['static/*', 'templates/*']
+        'anacostia_pipeline': [*static_files, *template_files]
+    },
+    include_package_data=True,
+    exclude_package_data={
+        '': ['__pycache__', '*.pyc', '*.pyo']
     },
     install_requires=[
         "networkx==3.1",
