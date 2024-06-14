353d879d4984158fe6ab5bbc905401545f29e457
Minh Quan Do <mdo6180@gmail.com>
2024-06-14 19:01:39 -0400

    both root and leaf services now log to the same log file

--- tests/distributed_test/leaf-service/src/main.py
+++ tests/distributed_test/leaf-service/src/main.py
@@ -1,14 +1,28 @@
 from typing import Union
-
+import logging
+import shutil
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+leaf_test_path = "/testing_artifacts"
+
+log_path = f"{leaf_test_path}/anacostia.log"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='LEAF %(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename=log_path,
+    filemode='a'
+)
+logger = logging.getLogger(__name__)
+
 
 @app.get("/")
 def read_root():
+    logger.debug("hello from leaf service")
     return {"Hello": "leaf World"}
-
 
 
 @app.get("/items/{item_id}")
--- tests/distributed_test/root-service/src/main.py
+++ tests/distributed_test/root-service/src/main.py
@@ -1,14 +1,29 @@
 from typing import Union
-
+import logging
+import shutil
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+root_test_path = "/testing_artifacts"
+
+log_path = f"{root_test_path}/anacostia.log"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='ROOT %(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename=log_path,
+    filemode='a'
+)
+logger = logging.getLogger(__name__)
+
+
 
 @app.get("/")
 def read_root():
+    logger.debug("hello from root service")
     return {"Hello": "Root World"}
-
 
 
 @app.get("/items/{item_id}")
