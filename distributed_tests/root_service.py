c9a958c2a83a3b0e6dcdc3829d0f24b1f017a1b1
Minh Quan Do <mdo6180@gmail.com>
2024-09-15 03:55:00 -0400

    moved service.py into the subapps folder

--- distributed_tests/root_service.py
+++ distributed_tests/root_service.py
@@ -12,7 +12,7 @@ from anacostia_pipeline.engine.base import BaseNode, BaseActionNode, BaseMetadat
 from anacostia_pipeline.resources.filesystem_store import FilesystemStoreNode
 from anacostia_pipeline.metadata.sql_metadata_store import SqliteMetadataStore
 from anacostia_pipeline.engine.pipeline import Pipeline
-from anacostia_pipeline.dashboard.service import RootService
+from anacostia_pipeline.dashboard.subapps.service import RootService
 from anacostia_pipeline.actions.network import SenderNode
 from anacostia_pipeline.dashboard.subapps.pipeline import RootPipelineWebserver
 
--- distributed_tests/tests/service_test.py
+++ distributed_tests/tests/service_test.py
@@ -1,6 +1,6 @@
 from anacostia_pipeline.engine.pipeline import Pipeline, PipelineModel
 from anacostia_pipeline.dashboard.subapps.pipeline import RootPipelineWebserver
-from anacostia_pipeline.dashboard.service import AnacostiaService
+from anacostia_pipeline.dashboard.subapps.service import AnacostiaService
 
 
 if __name__ == "__main__":
