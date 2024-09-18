c4195f03bf77b388a1dfcc9e48d67bc6fb684ec6
Minh Quan Do <mdo6180@gmail.com>
2024-09-18 02:05:17 -0400

    deleted some unecessary files

--- distributed_tests/tests/service_test.py
+++
@@ -1,8 +0,0 @@
-from anacostia_pipeline.engine.pipeline import Pipeline, PipelineModel
-from anacostia_pipeline.dashboard.subapps.pipeline import RootPipelineWebserver
-from anacostia_pipeline.dashboard.subapps.service import AnacostiaService
-
-
-if __name__ == "__main__":
-    service = AnacostiaService(host="localhost", port=8001)
-    service.run()
