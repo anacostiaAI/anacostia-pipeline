a636b8e79f3ea4af6cf44bfafad8d0ee2ef64a48
Minh Quan Do <mdo6180@gmail.com>
2024-10-12 18:00:10 -0400

    verified changes works with root service

--- distributed_tests/root_service.py
+++ distributed_tests/root_service.py
@@ -6,8 +6,8 @@ import argparse
 import logging
 
 from anacostia_pipeline.engine.base import BaseActionNode, BaseMetadataStoreNode
-from anacostia_pipeline.resources.filesystem_store import FilesystemStoreNode
-from anacostia_pipeline.metadata.sql_metadata_store import SqliteMetadataStore
+from anacostia_pipeline.resources.filesystem.node import FilesystemStoreNode
+from anacostia_pipeline.metadata.sqlite.node import SqliteMetadataStore
 from anacostia_pipeline.engine.pipeline import Pipeline
 from anacostia_pipeline.dashboard.subapps.service import RootService
 from anacostia_pipeline.engine.network import SenderNode
