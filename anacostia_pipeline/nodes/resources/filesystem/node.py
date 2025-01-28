d21a1fedbb4654401fdca3df8743a883b2684965
Minh Quan Do <mdo6180@gmail.com>
2025-01-28 18:46:28 -0500

    added msvcrt library to support windows

--- anacostia_pipeline/nodes/resources/filesystem/node.py
+++ anacostia_pipeline/nodes/resources/filesystem/node.py
@@ -4,8 +4,12 @@ from datetime import datetime
 from logging import Logger
 from threading import Thread
 import traceback
-import fcntl
 from contextlib import contextmanager
+
+if os.name == 'nt':  # Windows
+    import msvcrt
+else:  # Unix-like systems (Linux, macOS)
+    import fcntl
 
 from anacostia_pipeline.nodes.resources.node import BaseResourceNode
 from anacostia_pipeline.nodes.metadata.node import BaseMetadataStoreNode
@@ -22,11 +26,27 @@ class EntryNotFoundError(Exception):
 @contextmanager
 def locked_file(filename, mode='r'):
     with open(filename, mode) as file:
-        fcntl.flock(file.fileno(), fcntl.LOCK_SH if mode.startswith('r') else fcntl.LOCK_EX)
         try:
+            if os.name == 'nt':  # Windows
+                if mode.startswith('r'):  # Shared lock for reading
+                    msvcrt.locking(file.fileno(), msvcrt.LK_NBRLCK, os.path.getsize(filename))
+                else:  # Exclusive lock for writing
+                    msvcrt.locking(file.fileno(), msvcrt.LK_LOCK, os.path.getsize(filename))
+            
+            else:  # Unix-like systems
+                if mode.startswith('r'):  # Shared lock for reading
+                    fcntl.flock(file.fileno(), fcntl.LOCK_SH)
+                else:  # Exclusive lock for writing
+                    fcntl.flock(file.fileno(), fcntl.LOCK_EX)
+            
             yield file
+        
         finally:
-            fcntl.flock(file.fileno(), fcntl.LOCK_UN)
+            # Unlock the file
+            if os.name == 'nt':  # Windows
+                msvcrt.locking(file.fileno(), msvcrt.LK_UNLCK, os.path.getsize(filename))
+            else:  # Unix-like systems
+                fcntl.flock(file.fileno(), fcntl.LOCK_UN)
 
         # use a shared lock (fcntl.LOCK_SH) for reading:
         # - allows multiple processes to acquire a shared lock for reading
