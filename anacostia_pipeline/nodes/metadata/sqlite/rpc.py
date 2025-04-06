643d6d7b6b83f757b57e06b69775d09b34e3908e
Minh Quan Do <mdo6180@gmail.com>
2025-04-05 20:09:26 -0400

    added /get_run_id/ to sqlite rpc

--- anacostia_pipeline/nodes/metadata/sqlite/rpc.py
+++ anacostia_pipeline/nodes/metadata/sqlite/rpc.py
@@ -13,6 +13,11 @@ class SqliteMetadataRPCCallee(BaseMetadataRPCCallee):
         super().__init__(metadata_store, caller_url, host, port, loggers, *args, **kwargs)
         self.metadata_store = metadata_store
 
+        @self.get("/get_run_id/")
+        async def get_run_id():
+            run_id = self.metadata_store.get_run_id()
+            return {"run_id": run_id}
+
         @self.get("/get_node_id/")
         async def get_node_id(node_name: str):
             node_id = self.metadata_store.get_node_id(node_name)
@@ -59,6 +64,12 @@ class SqliteMetadataRPCCaller(BaseMetadataRPCCaller):
     def __init__(self, caller_name, caller_host = "127.0.0.1", caller_port = 8000, loggers = None, *args, **kwargs):
         super().__init__(caller_name, caller_host, caller_port, loggers, *args, **kwargs)
     
+    async def get_run_id(self):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{self.get_callee_url()}/get_run_id/")
+            run_id = response.json()["run_id"]
+            return run_id
+
     async def get_node_id(self, node_name: str):
         async with httpx.AsyncClient() as client:
             response = await client.get(f"{self.get_callee_url()}/get_node_id/?node_name={node_name}")
