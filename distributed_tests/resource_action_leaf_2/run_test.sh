0a952d4817c5183199625a68b080cc026c424cce
Minh Quan Do <mdo6180@gmail.com>
2025-08-20 00:56:01 -0400

    made changes to resource_action_leaf_2

--- distributed_tests/resource_action_leaf_2/run_test.sh
+++ distributed_tests/resource_action_leaf_2/run_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # remember to make file executable with chmod +x run_test.sh
+# generate certs
+mkdir -p ./certs
+mkcert -key-file ./certs/private_leaf.key -cert-file ./certs/certificate_leaf.pem localhost 127.0.0.1
+mkcert -key-file ./certs/private_root.key -cert-file ./certs/certificate_root.pem localhost 127.0.0.1
 
 # Configuration
 ROOT_SCRIPT="root.py"
